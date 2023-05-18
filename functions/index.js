0eb7959767e5e12b13e8d33b81979b4acaf342c0
Roman Furman <roman.furman@uptech.team>
2023-05-18 17:39:31 +0300

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require("firebase-functions");
 
 exports.helloWorld = functions.https.onRequest(async (request, response) => {
+    // check steps.js for examples
     response.json({ message: "Hello from function!" });
 });
     
